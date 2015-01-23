169180900587079cec67c90bdc2cf2b6b5d295f7
Park June Chul <pjc0247@naver.com>
2015-01-23 20:51:24 +0900

    Update src.rb

--- src.rb
+++ src.rb
@@ -5,7 +5,7 @@ def break_hangul c
   joong = ((code-jong) / 28) % 21
   cho = ((code-jong) / 28 - joong) / 21
   
-  [(cho+0x1100).chr('utf-8'),(joong+0x1161).chr('utf-8'),(jong+0x11A7).chr('utf-8')]
+  [(cho+0x1100),(joong+0x1161),(jong+0x11A7)].map {|c| c.chr('utf-8')}
 end
 
 '안녕월드'.each_char do |b|
